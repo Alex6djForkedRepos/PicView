41e6babb22c8e3c2be5822286c8ed11b6f74a53f
Ruben <Ruben2776@users.noreply.github.com>
2025-11-05 22:31:48 +0100

    Fix delayed centering at start-up

--- src/PicView.Avalonia/WindowBehavior/WindowFunctions.cs
+++ src/PicView.Avalonia/WindowBehavior/WindowFunctions.cs
@@ -357,7 +357,7 @@ public static class WindowFunctions
             window.Position = horizontal
                 ? new PixelPoint((int)centeredX, (int)centeredY)
                 : new PixelPoint(window.Position.X, (int)centeredY);
-        }, DispatcherPriority.Background);
+        });
     }
 
     public static void InitializeWindowSizeAndPosition(Window window)
